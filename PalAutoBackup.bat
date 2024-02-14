30dd66a5d464acc113515f22b2bc7df35dfa0fa8
Kira5033 <kira5033@gmail.com>
2024-02-14 20:44:43 +0800

    refactor: BACKUP_LOG is file name only

--- PalAutoBackup.bat
+++ PalAutoBackup.bat
@@ -1,11 +1,11 @@
 @echo off
 
 @rem 設定PalServer路徑
-set WORKDIR=D:\Steam\steamapps\common\PalServer
+set WORKDIR=D:\Steam\steamapps\common\PalServer\Pal\Saved\SaveGames\0
 @rem 設定備份路徑
 set BACKUPDIR=D:\Steam\steamapps\common\PalServer\Backup
-@rem 設定備份檔案
-set BACKUP_LOG=%BACKUPDIR%\00_backup_log.txt
+@rem 設定備份Log
+set BACKUP_LOG=00_backup_log.txt
 
 @rem 設定時間參數
 set CUR_YYYY=%date:~0,4%
@@ -16,14 +16,14 @@ if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
 set CUR_NN=%time:~3,2%
 set CUR_SS=%time:~6,2%
 set CUR_MS=%time:~9,2%
-set SUBFILENAME=%CUR_YYYY%_%CUR_MM%_%CUR_DD%_%CUR_HH%_%CUR_NN%_%CUR_SS%
+set TEMPFOLDER=%CUR_YYYY%_%CUR_MM%_%CUR_DD%_%CUR_HH%_%CUR_NN%_%CUR_SS%
 
 @rem 檢查備份資料夾是否存在
 if not exist %BACKUPDIR%\ (mkdir %BACKUPDIR%\)
 
 @rem 開始進行備份
-echo Start Backup >> %BACKUP_LOG%
-echo Create Folder - %SUBFILENAME% >> %BACKUP_LOG%
-mkdir %BACKUPDIR%\%SUBFILENAME%
-xcopy %WORKDIR%\Pal\Saved\SaveGames\0 %BACKUPDIR%\%SUBFILENAME% /E >> %BACKUP_LOG%
-echo Finish Backup >> %BACKUP_LOG%
+echo Start Backup >> %BACKUPDIR%\%BACKUP_LOG%
+echo Create Folder - %TEMPFOLDER% >> %BACKUPDIR%\%BACKUP_LOG%
+mkdir %BACKUPDIR%\%TEMPFOLDER%
+xcopy %WORKDIR% %BACKUPDIR%\%TEMPFOLDER% /E >> %BACKUPDIR%\%BACKUP_LOG%
+echo Finish Backup >> %BACKUPDIR%\%BACKUP_LOG%
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # PalBackup
-Scripts for backup Palworld
+
+簡易備份腳本，需搭配Windows工作排程器
+
+## 存檔路徑
+請直接將要備份的路徑找出來並設定上去
+```batch
+set WORKDIR=D:\Steam\steamapps\common\PalServer\Pal\Saved\SaveGames\0
+```
+
+## 備份路徑
+設定你的備份路徑
+```batch
+set BACKUPDIR=D:\Steam\steamapps\common\PalServer\Backup
+```
+
+## 備份LOG紀錄
+設定Log檔名，可使用預設即可
+```batch
+set BACKUP_LOG=00_backup_log.txt
+```
