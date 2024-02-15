7b99e8ab7ba0d8910482da06c19a54b6ce25dfa3
Kira5033 <kira5033@gmail.com>
2024-02-16 00:02:26 +0800

    fix: directory variable use quotation mark

--- PalAutoBackup.bat
+++ PalAutoBackup.bat
@@ -1,13 +1,14 @@
 @echo off
+setlocal enabledelayedexpansion
 
-@rem 設定PalServer路徑
-set WORKDIR=D:\Steam\steamapps\common\PalServer\Pal\Saved\SaveGames\0
-@rem 設定備份路徑
-set BACKUPDIR=D:\Steam\steamapps\common\PalServer\Backup
-@rem 設定備份Log
+:: 設定PalServer存檔路徑
+set WORKDIR="D:\Steam\steamapps\common\PalServer\Pal\Saved\SaveGames\0"
+:: 設定備份路徑
+set BACKUPDIR="D:\Steam\steamapps\common\PalServer\Backup"
+:: 設定備份Log
 set BACKUP_LOG=00_backup_log.txt
 
-@rem 設定時間參數
+:: 設定時間參數
 set CUR_YYYY=%date:~0,4%
 set CUR_MM=%date:~5,2%
 set CUR_DD=%date:~8,2%
@@ -18,12 +19,17 @@ set CUR_SS=%time:~6,2%
 set CUR_MS=%time:~9,2%
 set TEMPFOLDER=%CUR_YYYY%_%CUR_MM%_%CUR_DD%_%CUR_HH%_%CUR_NN%_%CUR_SS%
 
-@rem 檢查備份資料夾是否存在
-if not exist %BACKUPDIR%\ (mkdir %BACKUPDIR%\)
+:: 檢查備份資料夾是否存在，不存在則建立
+if not exist "%BACKUPDIR%\" (
+    mkdir "%BACKUPDIR%\"
+)
 
-@rem 開始進行備份
-echo Start Backup >> %BACKUPDIR%\%BACKUP_LOG%
-echo Create Folder - %TEMPFOLDER% >> %BACKUPDIR%\%BACKUP_LOG%
-mkdir %BACKUPDIR%\%TEMPFOLDER%
-xcopy %WORKDIR% %BACKUPDIR%\%TEMPFOLDER% /E >> %BACKUPDIR%\%BACKUP_LOG%
-echo Finish Backup >> %BACKUPDIR%\%BACKUP_LOG%
+:: 開始進行備份
+echo Start Backup >> "%BACKUPDIR%\%BACKUP_LOG%"
+echo Create Folder - %TEMPFOLDER% >> "%BACKUPDIR%\%BACKUP_LOG%"
+mkdir "%BACKUPDIR%\%TEMPFOLDER%"
+xcopy "%WORKDIR%" "%BACKUPDIR%\%TEMPFOLDER%" /E /Y >> "%BACKUPDIR%\%BACKUP_LOG%"
+
+echo Finish Backup >> "%BACKUPDIR%\%BACKUP_LOG%"
+
+endlocal
