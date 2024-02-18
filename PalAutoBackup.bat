43c0e8e0277da3eb829c7ff9199d83d2751d7fe7
Kira5033 <kira5033@gmail.com>
2024-02-18 08:51:04 +0800

    feat: remove old backup

--- PalAutoBackup.bat
+++ PalAutoBackup.bat
@@ -1,12 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: 設定PalServer存檔路徑
+:: 設定存檔路徑
 set WORKDIR="D:\Steam\steamapps\common\PalServer\Pal\Saved\SaveGames\0"
 :: 設定備份路徑
 set BACKUPDIR="D:\Steam\steamapps\common\PalServer\Backup"
-:: 設定備份Log
+:: 設定Log名稱
 set BACKUP_LOG=00_backup_log.txt
+:: 存檔保留天數
+set KEEPFILEDAY=7
 
 :: 設定時間參數
 set CUR_YYYY=%date:~0,4%
@@ -18,6 +20,7 @@ set CUR_NN=%time:~3,2%
 set CUR_SS=%time:~6,2%
 set CUR_MS=%time:~9,2%
 set TEMPFOLDER=%CUR_YYYY%_%CUR_MM%_%CUR_DD%_%CUR_HH%_%CUR_NN%_%CUR_SS%
+set TIMESTAMP=%CUR_YYYY%-%CUR_MM%-%CUR_DD% %CUR_HH%:%CUR_NN%:%CUR_SS%
 
 :: 檢查備份資料夾是否存在，不存在則建立
 if not exist "%BACKUPDIR%\" (
@@ -25,11 +28,22 @@ if not exist "%BACKUPDIR%\" (
 )
 
 :: 開始進行備份
-echo Start Backup >> "%BACKUPDIR%\%BACKUP_LOG%"
-echo Create Folder - %TEMPFOLDER% >> "%BACKUPDIR%\%BACKUP_LOG%"
+echo [%TIMESTAMP%] Start Backup >> "%BACKUPDIR%\%BACKUP_LOG%"
 mkdir "%BACKUPDIR%\%TEMPFOLDER%"
 xcopy "%WORKDIR%" "%BACKUPDIR%\%TEMPFOLDER%" /E /Y >> "%BACKUPDIR%\%BACKUP_LOG%"
+powershell -Command "(Get-Item '%BACKUPDIR%\%TEMPFOLDER%').LastWriteTime = Get-Date('%TIMESTAMP%')"
 
-echo Finish Backup >> "%BACKUPDIR%\%BACKUP_LOG%"
+:: 確認有備份內有無檔案
+FORFILES /P "%BACKUPDIR%\%TEMPFOLDER%" /C "cmd /c if @isdir == TRUE echo @path"
+if %errorlevel% NEQ 0 (
+    echo [%TIMESTAMP%] No files or directories found >> "%BACKUPDIR%\%BACKUP_LOG%"
+) else (
+    echo [%TIMESTAMP%] Backup Success >> "%BACKUPDIR%\%BACKUP_LOG%"
+)
+
+:: 移除舊備份
+echo [%TIMESTAMP%] Remove old backup >> "%BACKUPDIR%\%BACKUP_LOG%"
+FORFILES /P "%BACKUPDIR%" -D -%KEEPFILEDAY% /C "cmd /c if @isDir == TRUE echo @path & rd /s /q @path" >> "%BACKUPDIR%\%BACKUP_LOG%"
+echo [%TIMESTAMP%] Done >> "%BACKUPDIR%\%BACKUP_LOG%"
 
 endlocal
--- README.md
+++ README.md
@@ -5,17 +5,22 @@
 ## 存檔路徑
 請直接將要備份的路徑找出來並設定上去
 ```batch
-set WORKDIR=D:\Steam\steamapps\common\PalServer\Pal\Saved\SaveGames\0
+set WORKDIR="D:\Steam\steamapps\common\PalServer\Pal\Saved\SaveGames\0"
 ```
 
 ## 備份路徑
 設定你的備份路徑
 ```batch
-set BACKUPDIR=D:\Steam\steamapps\common\PalServer\Backup
+set BACKUPDIR="D:\Steam\steamapps\common\PalServer\Backup"
 ```
 
-## 備份LOG紀錄
+## (option)Log檔名
 設定Log檔名，可使用預設即可
 ```batch
 set BACKUP_LOG=00_backup_log.txt
+```
+
+## 備份保留天數
+```batch
+set KEEPFILEDAY=7
 ```
